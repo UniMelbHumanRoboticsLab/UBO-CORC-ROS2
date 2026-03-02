cd3206be2c363bcb33b2999d3cd8f919d9edf5a7
jqloh@student.unimelb.edu.au <jqloh@student.unimelb.edu.au>
2026-03-02 18:05:46 +1100

    added external bias collection for the init_bias, bcs im lazy to make it one ubo gui :P

---
+++ pyCORC/gui/ubo/bias_gui.py
@@ -0,0 +1,375 @@
+print("Importing Paths")
+import os, sys,json
+for mod in [m for m in sys.modules if 'PyQt5' in m]:
+    del sys.modules[mod]
+    
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+import numpy as np
+np.set_printoptions(
+    precision=4,
+    linewidth=np.inf,   
+    formatter={'float_kind': lambda x: f"{x:.4f}"}
+)
+print("Importing Logger")
+from bias_logger import bias_logger
+print("Importing GUI")
+from base.pycorc_gui import pycorc_gui
+print("Importing IOs")
+from pycorc_io.corc.corc_FLNL_client import corc_FLNL_client
+from pycorc_io.xsens.xsens_server import xsens_server
+from pycorc_io.xsens.ub_pckg.ub import ub
+from pycorc_io.package_utils.unpack_json import get_subject_params
+print("Importing PySide6")
+from PySide6 import QtWidgets
+from PySide6.QtGui import QShortcut
+from PySide6.QtCore import QThread,Qt,QMetaObject,Slot,Qt
+
+NUM_RFT = 3
+rft_key = ["clav","ua","fa"]
+class bias_gui(pycorc_gui):
+    def __init__(self,init_args):
+        self.init_args = init_args
+        self.corc_args = self.init_args["init_flags"]["corc"]
+        self.xsens_args = self.init_args["init_flags"]["xsens"]
+        self.gui_args = self.init_args["init_flags"]["gui"]
+        self.log_args = self.init_args["init_flags"]["log"]
+        self.session_data = self.init_args["session_data"]
+        
+        body_path = os.path.join(os.path.dirname(__file__), '../../..',f'logs/pycorc_recordings/{self.session_data["exp_id"]}/subject_measurements/{self.session_data["subject_id"]}')
+        self.body_params_rbt,self.ft_grav,self.removed_bias = get_subject_params(body_path)
+        self.bias_response = np.array([0 for i in range(18)])
+        
+        for dict_type,param_dict in zip(["BODY PARAMS","FT_GRAV","REMOVED_BIAS"],[self.body_params_rbt,self.ft_grav,self.removed_bias]):
+            print()
+            print(dict_type)
+            for (key,value) in param_dict.items():
+                print(f"{key}:{value}")
+        print()
+
+        self.num_closed_threads = 0
+        self.num_opened_threads = 0
+        self.requested_threads = 3
+
+        super().__init__(freq=self.gui_args["freq"],gui_3d=self.gui_args["3d"])
+
+        if self.gui_args["force"]:
+            self.ubo_wrenches_live_stream = self.init_live_stream(num_columns=2)
+            
+        """
+        Initilization
+        """
+        self.init_IOs()
+        self.init_shortcuts()
+
+    """
+    Init Sensor Worker / Thread / GUI Helper Functions
+    """
+    def add_opened_threads(self):
+        self.num_opened_threads += 1
+        print(f"Threads Opened:{self.num_opened_threads}")
+        
+        if self.num_opened_threads == self.requested_threads:
+            QMetaObject.invokeMethod(self.logger_worker , "sensors_ready", Qt.ConnectionType.QueuedConnection)
+    def init_corc(self):
+        # init response label
+        self.corc_label = self.init_response_label(size=[525,200])
+        # init corc thread and worker
+        self.corc_thread = QThread()
+        self.corc_worker = corc_FLNL_client(ip=self.corc_args["ip"],port=self.corc_args["port"])
+        # init corc live stream plot
+        self.ubo_F_live_stream_plots = []
+        self.ubo_M_live_stream_plots = []
+        if self.gui_args["on"] and self.gui_args["force"]:
+            for i in range(NUM_RFT):
+                self.ubo_F_live_stream_plots.append(self.add_live_stream_plot(live_stream=self.ubo_wrenches_live_stream,sensor_name= f"UBO_{i} Force",unit="N",dim=3))
+                self.ubo_M_live_stream_plots.append(self.add_live_stream_plot(live_stream=self.ubo_wrenches_live_stream,sensor_name= f"UBO_{i} Moment",unit="Nm",dim=3))
+    def init_xsens(self):
+        # init response label
+        self.xsens_label = self.init_response_label(size=[300,200])
+        # init corc thread and worker
+        self.xsens_thread = QThread()
+        self.xsens_worker = xsens_server(ip=self.xsens_args["ip"],port=self.xsens_args["port"])
+    def init_skeleton(self):
+        # init ub model
+        self.skeleton = {
+            "right":{},
+            "left":{}
+        }
+        # init xsens skeleton for future use
+        for side,color in zip(["right","left"],["purple", "orange"]):
+            self.skeleton[side]["ub_xsens"] = ub(self.body_params_rbt,model="ubo",arm_side=side)
+            robot_joints, robot_ee = self.skeleton[side]["ub_xsens"].ub_fkine([0]*12)
+            
+            if self.gui_args["on"] and self.gui_args["3d"]:
+                if self.xsens_args["on"]:
+                    body = self.init_line(points=robot_joints.t,color=color)
+                    ees = [self.init_frame(pos=ee_pose.t,rot=ee_pose.R*0.1) for ee_pose in robot_ee]
+                    self.skeleton[side]["body"] = body
+                    self.skeleton[side]["ees"] = ees
+    
+                    if self.corc_args["on"]:
+                        if side == "right":
+                            forces = [0] + [self.init_line(points=np.vstack([ee_pose.t, ee_pose.t + np.matmul(ee_pose.R,np.array([0,0,0.2]))]),color="pink") for ee_pose in robot_ee[1:]]
+                        else:
+                            forces = [0 for ee_pose in robot_ee]
+                    else:
+                        forces = [0 for ee_pose in robot_ee]
+                    self.skeleton[side]["forces"] = forces
+    def init_logger(self):
+        # init response label
+        self.logger_label = self.init_response_label(size=[250,150])
+        self.logger_thread = QThread()
+        self.logger_worker = bias_logger(init_args = self.init_args["init_flags"],
+                                        session_data  = self.session_data)
+    def init_shortcuts(self):
+        # to start / stop logging at button press
+        if self.log_args["on"]:
+            # to start logging
+            start_log = QShortcut("S", self)
+            start_log.activated.connect(self.logger_worker.start_worker)
+            # to stop logging and close everything
+            stop_log = QShortcut("enter", self)
+            stop_log.activated.connect(self.logger_worker.reset_logger)
+        close = QShortcut("Q", self)
+        close.activated.connect(self.gui_timer.stop)
+        close.activated.connect(self.close_workers)
+        
+    def init_IOs(self):
+        """
+        Init IO thread and worker
+        """
+        self.init_skeleton()
+        if self.corc_args["on"]:
+            self.init_corc()
+        if self.xsens_args["on"]:
+            self.init_xsens()
+        if self.log_args["on"]:
+            self.init_logger()
+            self.logger_response = {
+            "print_text":"Idle\n",
+            "logger_fps":0.0
+        }
+        
+        """
+        Move sensor worker to thread 
+        Connect intersensor signals
+        Connect signals to sensor GUI callbacks
+        Connect thread start to start sensor worker, update number of open threads
+        Connect worker stop to stop thread at closeup
+        Connect thread finished to close app
+        """
+        if self.corc_args["on"]:
+            # move worker to thread
+            self.corc_worker.moveToThread(self.corc_thread)
+            # connect to gui
+            self.corc_worker.data_ready.connect(self.update_corc,type=Qt.ConnectionType.QueuedConnection)
+            # connect thread start to start worker   
+            self.corc_thread.started.connect(self.corc_worker.start_worker)
+            self.corc_thread.started.connect(self.add_opened_threads)
+            # connect worker stop to stop thread at closeup
+            self.corc_worker.stopped.connect(self.corc_thread.exit)
+            # connect thread finished to close app
+            self.corc_thread.finished.connect(self.close_app)
+        if self.xsens_args["on"]:
+            # move worker to thread
+            self.xsens_worker.moveToThread(self.xsens_thread)
+            # connect to gui
+            self.xsens_worker.data_ready.connect(self.update_xsens,type=Qt.ConnectionType.QueuedConnection)
+            # connect thread start to start worker   
+            self.xsens_thread.started.connect(self.xsens_worker.start_worker)
+            self.xsens_thread.started.connect(self.add_opened_threads)
+            # connect worker stop to stop thread at closeup
+            self.xsens_worker.stopped.connect(self.xsens_thread.exit)
+            # connect thread finished to close app
+            self.xsens_thread.finished.connect(self.close_app)
+        if self.log_args["on"]:
+            # move worker to thread
+            self.logger_worker.moveToThread(self.logger_thread)
+            # connect corc and xsens to logger
+            if self.corc_args["on"]:
+                self.corc_worker.data_ready.connect(self.logger_worker.update_corc,type=Qt.ConnectionType.QueuedConnection)
+            if self.xsens_args["on"]:
+                self.xsens_worker.data_ready.connect(self.logger_worker.update_xsens,type=Qt.ConnectionType.QueuedConnection)
+            # connect logger to sensor gui
+            self.logger_worker.time_ready.connect(self.update_logger,type=Qt.ConnectionType.QueuedConnection)
+            self.logger_worker.bias_ready.connect(self.update_bias,type=Qt.ConnectionType.QueuedConnection)
+            # connect thread start to add opened threads  
+            self.logger_thread.started.connect(self.add_opened_threads)
+            # connect worker stop to stop thread at closeup
+            self.logger_worker.stopped.connect(self.logger_thread.exit)
+            # connect worker collect finish to stop app
+            self.logger_worker.collect_finish.connect(self.gui_timer.stop)
+            self.logger_worker.collect_finish.connect(self.close_workers)
+            # connect thread finished to close app
+            self.logger_thread.finished.connect(self.close_app)
+
+        """
+        Start threads
+        """
+        if self.corc_args["on"]:
+            self.corc_thread.start()
+            self.corc_thread.setPriority(QThread.Priority.TimeCriticalPriority)
+        if self.xsens_args["on"]:
+            self.xsens_thread.start()
+            self.xsens_thread.setPriority(QThread.Priority.TimeCriticalPriority)
+        if self.log_args["on"]:
+            self.logger_thread.start()
+            self.logger_thread.setPriority(QThread.Priority.TimeCriticalPriority)
+        
+    """
+    Update Sensor Variable Callbacks
+    """
+    @Slot(dict)
+    def update_corc(self,corc_response):
+        self.corc_response = corc_response
+    def update_xsens(self,xsens_response):
+        self.xsens_response = xsens_response
+    @Slot(dict)
+    def update_logger(self,logger_response):
+        self.logger_response = logger_response
+    def update_bias(self,bias_response):
+        self.bias_response = bias_response["bias"]
+
+    """
+    Update Main GUI Helper Functions and Callback
+    """
+    def update_gui(self):
+        super().update_gui()
+        if self.gui_args["on"]:
+            # update live stream time buffer
+            if self.gui_args["force"]:
+                self.update_live_stream_buffer(live_stream=self.ubo_wrenches_live_stream)
+        if hasattr(self, 'corc_response'):
+            # update rft info
+            corc_data = self.corc_response["raw_data"]
+            fps = self.corc_response["corc_fps"]
+            txt = ""
+
+            # get the orientation of the sensors if exist:
+            if hasattr(self, 'xsens_response'):
+                robot_joints, robot_ee = self.skeleton["right"]["ub_xsens"].ub_fkine(self.xsens_response["right"]["list"])
+            
+            for i in range(NUM_RFT):
+                # add back the initial removed sensor base bias and remove the subsequent installation bias obtained from the logger
+                cur_removed_bias = np.array(self.removed_bias[rft_key[i]])
+                force_data = np.array(corc_data[1+i*6:1+i*6+6])+cur_removed_bias-self.bias_response[i*6:i*6+6]
+                
+                # weight compensate with if robot_ee exist
+                if hasattr(self, 'xsens_response'):
+                    pose = robot_ee[i + 1]
+                    weight_comp = [x * self.ft_grav[rft_key[i]] for x in [0,0,-1]]
+                    force_data = force_data - np.array(list(np.matmul(pose.R.T, np.array(weight_comp))) + [0,0,0])
+
+                txt += f"UBO_{i} Force(N): {force_data[0]:8.4f} {force_data[1]:8.4f} {force_data[2]:8.4f} | Moment(Nm): {force_data[3]:8.4f} {force_data[4]:8.4f} {force_data[5]:8.4f}\n"
+                txt += f"Force Mag{np.linalg.norm(force_data[:3]):8.4f} N | Moment Mag:{np.linalg.norm(force_data[3:]):8.4f} Nm\n"
+
+                if self.gui_args["on"] and self.gui_args["force"]:
+                    self.update_live_stream_plot(self.ubo_wrenches_live_stream,self.ubo_F_live_stream_plots[i],force_data,dim=3)
+                    self.update_live_stream_plot(self.ubo_wrenches_live_stream,self.ubo_M_live_stream_plots[i],force_data[3:],dim=3)
+            self.update_response_label(self.corc_label,f"FPS:{fps}\nCORC Running Time:{corc_data[0]}s\n{txt}")
+
+        if hasattr(self, 'xsens_response'):
+            # update rft info
+            timecode = self.xsens_response["timecode"]
+            fps = self.xsens_response["xsens_fps"]
+            right_list = self.xsens_response["right"]["list"]
+            left_list = self.xsens_response["left"]["list"]
+
+            txt = f"XSENS Timecode:{timecode}\n"
+
+            for i,key in  enumerate(['trunk_ie','trunk_aa','trunk_fe',
+                        'clav_dep_ev','clav_prot_ret',
+                        'shoulder_fe','shoulder_aa','shoulder_ie',
+                        'elbow_fe','elbow_ps','wrist_fe','wrist_dev']):
+                txt += f"{key:15}: {left_list[i]:8.4f} {right_list[i]:8.4f}\n"
+            self.update_response_label(self.xsens_label,f"FPS:{fps}\n{txt}")
+
+            if self.gui_args["on"] and self.gui_args["3d"]:
+                for side in ["right","left"]:
+                    robot_joints, robot_ee = self.skeleton[side]["ub_xsens"].ub_fkine(self.xsens_response[side]["list"])                        
+                    self.update_line(self.skeleton[side]["body"],points=robot_joints.t)
+                    
+                    for i,(frame,force,pose) in enumerate(zip(self.skeleton[side]["ees"],self.skeleton[side]["forces"],robot_ee)):
+                        self.update_frame(frame,pos=pose.t,rot=pose.R*0.1)
+                        if i != 0 and side == "right" and hasattr(self, 'corc_response'):
+                            j = i - 1
+                            # add back the initial removed sensor base bias and remove the subsequent installation bias obtained from the logger
+                            net_bias = np.array(self.removed_bias[rft_key[j]])-self.bias_response[j*6:j*6+6]
+                            force_data = np.array(corc_data[1+j*6:1+j*6+6])+np.array(net_bias)
+
+                            weight_comp = [x * self.ft_grav[rft_key[j]] for x in [0,0,-1]]
+                            force_data = force_data - np.array(list(np.matmul(pose.R.T, np.array(weight_comp))) + [0,0,0])
+                            self.update_line(force,points=np.vstack([pose.t, pose.t + np.matmul(pose.R,force_data[:3])*0.02]))
+        if hasattr(self, 'logger_response'):
+            print_text = self.logger_response["print_text"]
+            fps = self.logger_response["logger_fps"]
+            self.update_response_label(self.logger_label,f"{print_text}FPS:{fps}")
+    
+    """
+    Cleanup
+    """
+    def close_worker_thread(self,worker):
+        QMetaObject.invokeMethod(worker, "stop", Qt.ConnectionType.QueuedConnection)
+    def close_workers(self):
+        if hasattr(self, 'corc_response'):
+            self.close_worker_thread(self.corc_worker)
+        if hasattr(self, 'xsens_response'):
+            self.close_worker_thread(self.xsens_worker)
+        if hasattr(self, 'logger_response'):
+            self.close_worker_thread(self.logger_worker)
+    @Slot()
+    def close_app(self):
+        self.num_closed_threads += 1
+        if self.num_closed_threads == self.num_opened_threads:
+            print("Shutting down app...")
+            for plt in self.plt_items:
+                plt.clear()
+                plt.deleteLater()
+            self.close()
+            self.deleteLater()
+
+if __name__ == "__main__":
+    try:
+        argv = sys.argv[1]
+    except:
+        argv ={
+               "init_flags":{"corc":{"on":True,
+                                     "ip":"127.0.0.1",
+                                     "port":2048},
+                            "xsens":{"on":True,
+                                     "ip":"0.0.0.0",
+                                     "port":9764},
+                             "gui":{"on":True,
+                                    "freq":30,
+                                    "3d":True,
+                                    "force":False},
+
+                             "log":{"on":True}
+                             },
+                "session_data":{
+                    "exp_id":"exp1",
+                    "subject_id":"ying3",
+                }
+               }
+        
+        argv = json.dumps(argv)
+
+    init_args = json.loads(argv)
+    app = QtWidgets.QApplication(sys.argv)
+    w = bias_gui(init_args)
+    
+    title_str = f"UBO-CORC-{init_args['session_data']['exp_id']}"+"-"
+    title_str += init_args['session_data']['subject_id']+"-"
+    title_str += "BIAS"+" "
+    w.setWindowTitle(title_str)
+    w.show()
+
+    app.exec()
+    app.quit()
+    import shiboken6
+    shiboken6.delete(app)  # Immediate deletion
+    sys.exit()
+
+
+    # 169.254.45.31
---
+++ pyCORC/gui/ubo/bias_logger.py
@@ -0,0 +1,208 @@
+import os
+import numpy as np
+import pandas as pd
+
+from pycorc_io.package_utils.unpack_json import get_subject_params
+from pycorc_io.xsens.ub_pckg.ub import ub
+from PySide6.QtCore import QObject, Signal,QTimer,Slot,QElapsedTimer,Qt
+NUM_RFT = 3
+rft_key = ["clav","ua","fa"]
+class bias_logger(QObject):
+    time_ready = Signal(dict)
+    bias_ready = Signal(dict)
+    collect_finish = Signal()
+    stopped = Signal()
+    def __init__(self,init_args,session_data):
+        super().__init__()
+        self.init_args = init_args
+        self.exp_id = session_data["exp_id"]
+        self.subject_id = session_data["subject_id"]
+
+        self.body_path = os.path.join(os.path.dirname(__file__), '../../..',f'logs/pycorc_recordings/{self.exp_id}/subject_measurements/{self.subject_id}')
+        self.body_params_rbt,self.ft_grav,self.removed_bias = get_subject_params(self.body_path)
+        self.skeleton = ub(self.body_params_rbt,model="ubo",arm_side="right")
+        self.sensors_ready_flag = False
+        self.collect_bias = True
+        
+        # FPS Calculator
+        self.logger_frame_count = 0
+        self.logger_fps = 0
+        self.logger_timer = QElapsedTimer() # use the system clock
+        self.logger_timer.start()
+        self.logger_cur_time = self.logger_timer.elapsed()
+        self.logger_last_time = self.logger_timer.elapsed()
+        self.elapsed_time = 0
+        
+        print("Bias Logger Started")
+    
+    """
+    Logging Callback
+    """
+    def log_current_data(self):
+        # update FPS
+        print_text = "Logging Bias\n"
+        self.logger_frame_count += 1
+        self.logger_cur_time = self.logger_timer.elapsed()
+        self.elapsed_time = (self.logger_cur_time-self.logger_start_time)/1000
+        print_text += f"Time Elapsed:{self.elapsed_time}\n"
+        if self.logger_cur_time-self.logger_last_time >= 1000:
+            self.logger_fps = self.logger_frame_count * 1000 / (self.logger_cur_time-self.logger_last_time)
+            self.logger_last_time = self.logger_cur_time
+            self.logger_frame_count = 0
+
+        if self.init_args["corc"]["on"] and hasattr(self, 'corc_response'):
+            print_text += f'CORC:{self.corc_response["corc_fps"]}\n'
+            self.corc_arr.append(self.corc_response["raw_data"]+[self.elapsed_time])
+
+        if self.init_args["xsens"]["on"] and hasattr(self, 'xsens_response'):
+            print_text += f'XSENS:{self.xsens_response["xsens_fps"]}\n'
+
+            timecode = self.xsens_response["timecode"]
+            right = self.xsens_response["right"]["list"]
+            left = self.xsens_response["left"]["list"]
+            self.xsens_arr.append([timecode]+right+left)
+        
+        """
+        TODO: 
+            use the xsens readings to calculate gravity bias
+            remove it from the corc reading to get rft bias at that sample point
+            append the rft bias arrar without any forces acting on it (including gravity)
+        """
+        if hasattr(self, 'corc_response') and hasattr(self, 'xsens_response') and self.collect_bias:
+            robot_joints, robot_ee = self.skeleton.ub_fkine(right)
+            corc_data =  self.corc_response["raw_data"]
+            bias_data = []
+            for i in range(NUM_RFT):
+                # add back the removed gravity bias
+                cur_removed_bias = np.array(self.removed_bias[rft_key[i]])
+                force_data = np.array(corc_data[1+i*6:1+i*6+6])+cur_removed_bias
+                
+                # weight compensate with if robot_ee exist to get the actual installation bias
+                pose = robot_ee[i + 1]
+                weight_comp = [x * self.ft_grav[rft_key[i]] for x in [0,0,-1]]
+                force_data = force_data - np.array(list(np.matmul(pose.R.T, np.array(weight_comp))) + [0,0,0])
+                bias_data += force_data.tolist()
+            self.bias_arr.append(bias_data)
+                
+        # emit the signal
+        data = {
+            "print_text":print_text,
+            "logger_fps":self.logger_fps
+        }
+        self.time_ready.emit(data)
+    
+    """
+    Initialization Callback
+    """
+    @Slot()
+    def start_worker(self):
+        if self.sensors_ready_flag:
+            self.poll_timer = QTimer()
+            self.poll_timer.setTimerType(Qt.PreciseTimer)
+            self.poll_timer.timeout.connect(self.log_current_data)
+            self.poll_timer.start(int(1/100*1000))
+            self.logger_start_time = self.logger_timer.elapsed()
+    
+            # corc
+            if self.init_args["corc"]["on"]:
+                self.corc_arr = []
+            # xsens
+            if self.init_args["xsens"]["on"]:
+                self.xsens_arr = []
+            # bias
+            if self.init_args["corc"]["on"] and self.init_args["xsens"]["on"]:
+                self.bias_arr = []
+        else:
+            data = {
+                "print_text":"Bro wait\n",
+                "logger_fps":0
+            }
+            self.time_ready.emit(data)
+        
+    """
+    External Signals Callbacks
+    """
+    @Slot()
+    def sensors_ready(self): # [self.rft_data_arr,self.rft_pose,self.rft_fps]
+        self.sensors_ready_flag = True
+        data = {
+            "print_text":"Sensors Ready\n",
+            "logger_fps":0
+        }
+        self.time_ready.emit(data)
+    def update_corc(self,corc_response): # [self.rft_data_arr,self.rft_pose,self.rft_fps]
+        self.corc_response = corc_response
+    @Slot(dict)
+    def update_xsens(self,xsens_response): # [self.rft_data_arr,self.rft_pose,self.rft_fps]
+        self.xsens_response = xsens_response
+    @Slot()
+    def stop(self):
+        if hasattr(self, 'poll_timer'):
+            self.poll_timer.stop()
+        self.stopped.emit()
+    @Slot()
+    def reset_logger(self):
+        if hasattr(self, 'corc_response') and hasattr(self, 'xsens_response') and self.collect_bias:
+            print(f"\nBias Take Elapsed Time: {self.elapsed_time}")
+            joints = ['trunk_ie','trunk_aa','trunk_fe',
+                      'clav_dep_ev','clav_prot_ret',
+                      'shoulder_fe','shoulder_aa','shoulder_ie',
+                      'elbow_fe','elbow_ps',
+                      'wrist_fe','wrist_dev']
+            xsens_column = ["timecode"]+[f"{joint}_{side}" for side in ["right","left"] for joint in joints]
+            corc_column = [
+                        "corc time",
+                        "F1x","F1y","F1z","T1x","T1y","T1z",
+                        "F2x","F2y","F2z","T2x","T2y","T2z",
+                        "F3x","F3y","F3z","T3x","T3y","T3z",
+                        "elapsed_time"
+                       ]
+            total_arr = np.hstack((np.array(self.xsens_arr),np.array(self.corc_arr)))
+            df = pd.DataFrame(total_arr,columns=xsens_column+corc_column)
+            self.save_file(path=f"{self.body_path}/",df=df,item="UBORecordBiasLog")
+            
+            """
+            emit the average installation bias for this variation to the GUI avd save everything as well
+            """
+            mean_bias = np.mean(np.array(self.bias_arr),axis=0)
+            for i in range(3):
+                bias = mean_bias[i*6:i*6+6]
+                
+                txt = ""
+                for w in bias:
+                    txt+=f"{w:.4f},"
+                print(f"Installation Bias{i}:",txt)
+                
+            self.bias_ready.emit({
+                "bias":mean_bias.tolist()
+                    })
+            
+            bias_column = [
+                        "F1x","F1y","F1z","T1x","T1y","T1z",
+                        "F2x","F2y","F2z","T2x","T2y","T2z",
+                        "F3x","F3y","F3z","T3x","T3y","T3z",
+                       ]
+            bias_arr = np.array(self.bias_arr)
+            df = pd.DataFrame(bias_arr,columns=bias_column)
+            self.save_file(path=f"{self.body_path}/",df=df,item="UBOConstBiasLog")
+            np.save(f"{self.body_path}/UBOAvgBias", mean_bias)
+            
+            print(f"Finished collecting {self.subject_id} Bias")
+            self.collect_bias = False
+
+            # reset everything
+            if self.init_args["corc"]["on"]:
+                self.corc_arr = []
+            if self.init_args["xsens"]["on"]:
+                self.xsens_arr = []
+            self.logger_start_time = self.logger_timer.elapsed()
+
+    """
+    Helper Function
+    """    
+    def save_file(self,path,df:pd.DataFrame,item):
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        df.to_csv(f"{path}/{item}.csv",index=False)    
+
+    
