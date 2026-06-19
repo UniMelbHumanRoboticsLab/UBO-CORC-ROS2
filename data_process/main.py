fix in time normalization to remove resampling error

data_process/main.py
@@ -18,8 +18,8 @@ from pyCORC.pycorc_io.xsens.ub_pckg.ub import ub
 from pyCORC.pycorc_io.package_utils.unpack_json import get_subject_params
 import FreeSimpleGUI as sg
 
-p_start = 2
-sub_start  = 14
+p_start = 1
+sub_start  = 11
 for p in range(p_start,4):
     if p == 1:
         sm_num = 2
@@ -103,6 +103,7 @@ for p in range(p_start,4):
                     #     print(f"Already removed {subject_path}/{var}/processed/index")    
                     print(f'{subject_path}/{var}/processed/UBORecord{rep}Log.csv Processed')
                     full_df = pd.read_csv(f'{subject_path}/{var}/processed/UBORecord{rep}Log.csv')
+                    print(f"Num of samples: {full_df.values.shape[0]}")
                     time_data_norm = full_df["norm_time"].values
                     q_traj_rad = full_df[q].values
                     qdot_traj_rad = full_df[qdot].values
@@ -112,9 +113,7 @@ for p in range(p_start,4):
                     if n == 0:
                         reference_interaction = np.hstack((q_traj_rad,qdot_traj_rad,tau_total))
                         reference_index = indices_traj
-                    if var == session_data["variants"][0] and n == 0:
-                        time_data       = time_data_norm
-                        num_samples     = time_data.shape[0]
+                        ref_time_data_norm = time_data_norm
                 else:
                     data_path = f'{subject_path}/{var}/raw/UBORecord{rep}Log.csv'
                     print("processing ",data_path)
@@ -196,28 +195,26 @@ for p in range(p_start,4):
                         taus_traj[rft_key]["filtered-rescaled"] = taus_traj[rft_key]["filtered-rescaled"][start_end_indices[0]:start_end_indices[-1],:]
                     
                     """ Time Normalization """
-                    # use first var first demo as the reference for overall time alignment
-                    if var == session_data["variants"][0] and n == 0:
-                        time_data_norm  = time_data / time_data[-1]
-                        time_data       = time_data_norm
-                        num_samples     = time_data.shape[0]
+                    # use first demo as the reference for overall time alignment
+                    # if var == session_data["variants"][0] and n == 0:
+                    if n == 0:
+                        ref_time_data_norm  = time_data / time_data[-1]
+                        ref_time_data = time_data
+                        time_data_norm       = ref_time_data_norm
                     else:
-                        time_data       = time_data / time_data[-1]
-                        time_data_norm  = np.linspace(0, 1, num_samples)
-                        actual_time_data = np.linspace(actual_time_data[0], actual_time_data[-1], num_samples)
+                        time_data_norm       = time_data / time_data[-1]
             
                     # rescale all data to the reference demo time
                     plot_results = False # check if time normalization fucks up the data
-                    corc_data       = rescale(time_data, corc_data,     time_data_norm, datatype=f"wrenches_norm_{var}-Rep{rep}",   plot_results=plot_results)
-                    q_traj          = rescale(time_data, q_traj,        time_data_norm, datatype=f"q_norm_{var}-Rep{rep}",          plot_results=plot_results)
-                    qdot_traj       = rescale(time_data, qdot_traj,     time_data_norm, datatype=f"qdot_norm_{var}-Rep{rep}",       plot_results=plot_results)
+                    # corc_data       = rescale(time_data_norm, corc_data,     ref_time_data_norm, datatype=f"wrenches_norm_{var}-Rep{rep}",   plot_results=plot_results)
+                    # q_traj          = rescale(time_data_norm, q_traj,        ref_time_data_norm, datatype=f"q_norm_{var}-Rep{rep}",          plot_results=plot_results)
+                    # qdot_traj       = rescale(time_data_norm, qdot_traj,     ref_time_data_norm, datatype=f"qdot_norm_{var}-Rep{rep}",       plot_results=plot_results)          
+                    # hand_pos_traj   = rescale(time_data_norm, hand_pos_traj, ref_time_data_norm, datatype=f"hand_pos_norm_{var}-Rep{rep}",   plot_results=plot_results)
+                    # hand_speed      = rescale(time_data_norm, hand_speed,    ref_time_data_norm, datatype=f"hand_speed_norm_{var}-Rep{rep}", plot_results=plot_results)
+                    # for rft_key in rft_keys+["total"]:
+                    #     taus_traj[rft_key]["filtered-rescaled"] = rescale(time_data_norm, taus_traj[rft_key]["filtered-rescaled"], time_data_norm, datatype=f"tau_norm_{rft_key}_{var}-Rep{rep}",    plot_results=plot_results)
                     q_traj_rad      = np.deg2rad(q_traj)
                     qdot_traj_rad   = np.deg2rad(qdot_traj)
-            
-                    hand_pos_traj   = rescale(time_data, hand_pos_traj, time_data_norm, datatype=f"hand_pos_norm_{var}-Rep{rep}",   plot_results=plot_results)
-                    hand_speed      = rescale(time_data, hand_speed,    time_data_norm, datatype=f"hand_speed_norm_{var}-Rep{rep}", plot_results=plot_results)
-                    for rft_key in rft_keys+["total"]:
-                        taus_traj[rft_key]["filtered-rescaled"] = rescale(time_data, taus_traj[rft_key]["filtered-rescaled"], time_data_norm, datatype=f"tau_norm_{rft_key}_{var}-Rep{rep}",    plot_results=plot_results)
                     
                     """ DTW Realignment """
                     # use first demo as the reference for the DTW alignment
@@ -231,7 +228,7 @@ for p in range(p_start,4):
                     paths = align_dtw(reference_interaction[:,0:align_dim],target_interaction[:,0:align_dim])
                     placehold = sg.popup('',location=(1550,300),non_blocking=True)  
                     f1,a1 = plot_multi_dim(
-                            [actual_time_data,actual_time_data,actual_time_data],
+                            [time_data,ref_time_data,ref_time_data],
                             [q_traj_rad,warp_target_to_ref(reference_interaction,q_traj_rad,paths),reference_interaction[:,0:10]],
                             10,
                             ["ori","dtw","ref"],
@@ -242,7 +239,7 @@ for p in range(p_start,4):
                             fig_label=f"joint angle rad {align_dim}",
                             show_stats=False,loc="+2000+800")
                     f2,a2 = plot_multi_dim(
-                            [actual_time_data,actual_time_data,actual_time_data],
+                            [time_data,ref_time_data,ref_time_data],
                             [qdot_traj_rad,warp_target_to_ref(reference_interaction,qdot_traj_rad,paths),reference_interaction[:,10:20]],
                             10,
                             ["ori","dtw","ref"],
@@ -254,7 +251,7 @@ for p in range(p_start,4):
                             show_stats=False,loc="+2900+800",
                             show_zero_cross=True)
                     f3,a3 = plot_multi_dim(
-                            [actual_time_data,actual_time_data,actual_time_data],
+                            [time_data,ref_time_data,ref_time_data],
                             [taus_traj["total"]["filtered-rescaled"],warp_target_to_ref(reference_interaction,taus_traj["total"]["filtered-rescaled"],paths),reference_interaction[:,20:]],
                             10,
                             ["ori","dtw","ref"],
@@ -272,8 +269,8 @@ for p in range(p_start,4):
                     """ Submovement Segment using unaligned movement"""
                     # segment rescaled but not aligned submovements
                     haih = actual_time_data[:np.newaxis]
-                    indices_traj,sbmvmt_indices = segment_sbmvmts(actual_time_data[:np.newaxis],hand_pos_traj,hand_speed,session_data["sbmvmt_num"],data_path=f'{subject_path}/{var}/processed/index/UBOIndex{rep}.txt',redo=False,skeleton={"ub":skeleton,"q":q_traj})
-                    placehold.close()
+                    indices_traj,sbmvmt_indices = segment_sbmvmts(actual_time_data[:np.newaxis],hand_pos_traj,hand_speed,session_data["sbmvmt_num"],data_path=f'{subject_path}/{var}/processed/index/UBOIndex{rep}.txt',redo=True,skeleton={"ub":skeleton,"q":q_traj})
+                    # placehold.close()
                     # f3,a3 = plot_multi_dim(
                     #         [actual_time_data,actual_time_data,actual_time_data],
                     #         [indices_traj,warp_target_to_ref(reference_interaction,indices_traj,paths),reference_index],
@@ -319,26 +316,28 @@ for p in range(p_start,4):
                         "task_params": task_params_b
                     }
                     tau_total = taus_traj["total"]["filtered-rescaled"]
-                    full_processed_data = np.hstack([actual_time_data[:,np.newaxis],time_data_norm[:,np.newaxis],indices_traj,q_traj_rad,qdot_traj_rad,tau_total,hand_pos_traj])
+                    t_warped =  warp_target_to_ref(reference_interaction,actual_time_data[:,np.newaxis],paths)
+                    indices_traj = warp_target_to_ref(reference_interaction,indices_traj,paths)
+                    full_processed_data = np.hstack([t_warped,ref_time_data_norm[:,np.newaxis],indices_traj,q_traj_rad,qdot_traj_rad,tau_total,hand_pos_traj])
                     
                     df_column = ["actual_time","norm_time","index"]+q+qdot+tau+["x","y","z"]
                     full_df = pd.DataFrame(full_processed_data,columns=df_column)
                     full_df.to_csv(f'{subject_path}/{var}/processed/UBORecord{rep}Log.csv',index=False)
         
                 # for plotting
-                time_list.append(time_data_norm)
+                time_list.append(ref_time_data_norm)
                 q_traj_list.append(q_traj_rad)
                 qdot_traj_list.append(qdot_traj_rad)
                 tau_traj_list.append(tau_total)
                 hand_3d_traj_list.append(hand_pos_traj)
                 sbmvmt_list.append(indices_traj)  
                 rep_label_list.append(f'{var}.Rep{rep}')
-        if exist:
-            # fig,ax = plot_3d_trajectory(traj_list=hand_3d_traj_list,label_list=rep_label_list,label=f"{session_data['subject_id']}_{session_data['patient_id']}")
-            # split_plot_all(session_data["variants"],time_list,q_traj_list,rep_label_list,rep_split=session_data["num_rep"],data_type="q_rad",fig_label=f"q_rad data {session_data['subject_id']}_{session_data['patient_id']}",plot=True)
-            # split_plot_all(session_data["variants"],time_list,sbmvmt_list,rep_label_list,rep_split=session_data["num_rep"],data_type="index",fig_label=f"index data {session_data['subject_id']}_{session_data['patient_id']}",plot=True)
+        if not exist:
+            fig,ax = plot_3d_trajectory(traj_list=hand_3d_traj_list,label_list=rep_label_list,label=f"{session_data['subject_id']}_{session_data['patient_id']}")
+            split_plot_all(session_data["variants"],time_list,q_traj_list,rep_label_list,rep_split=session_data["num_rep"],data_type="q_rad",fig_label=f"q_rad data {session_data['subject_id']}_{session_data['patient_id']}",plot=True)
+            split_plot_all(session_data["variants"],time_list,sbmvmt_list,rep_label_list,rep_split=session_data["num_rep"],data_type="index",fig_label=f"index data {session_data['subject_id']}_{session_data['patient_id']}",plot=True)
             split_plot_all(session_data["variants"],time_list,tau_traj_list,rep_label_list,rep_split=session_data["num_rep"],data_type="tau",fig_label=f"tau data {session_data['subject_id']}_{session_data['patient_id']}",plot=True)
-            # split_plot_all(session_data["variants"],time_list,qdot_traj_list,rep_label_list,rep_split=session_data["num_rep"],data_type="qdot_rad",fig_label=f"qdot_rad data {session_data['subject_id']}_{session_data['patient_id']}",plot=True)
+            split_plot_all(session_data["variants"],time_list,qdot_traj_list,rep_label_list,rep_split=session_data["num_rep"],data_type="qdot_rad",fig_label=f"qdot_rad data {session_data['subject_id']}_{session_data['patient_id']}",plot=True)
             plt.show()
             
 # plot_multi_dim(

data_process/post_process_pkg.py
@@ -23,7 +23,6 @@ def lpf(time_array,data,ts=0.01,fc=50,filter_type="low",datatype="trajectory",pl
     db_arr = []
 
     # Define the sos filter
-    
     fs = 1/ts  # Sampling frequency
     nyquist = fs / 2  # Nyquist frequency
     wc = fc / nyquist # Normalize cut-off frequency by Nyquist frequency (fs/2)
@@ -131,19 +130,24 @@ def segment_sbmvmts(time_array,hand_traj,hand_speed,submovement_num,data_path,re
                 indexed_time = np.array(lines[1].split(": ")[1].split(), dtype=float)
             else:
                 p = lines[1].split(": ")[1].split("\n")[0]
+                q = lines[2].split(": ")[1].split("\n")[0]
                 import re
                 indexed_time = np.array([float(x) for x in re.findall(r"\[([^\]]+)\]", p)])
                 if len(indexed_time) == 0:
                     indexed_time = np.array(lines[1].split(": ")[1].split(), dtype=float)
-                p = 0
+                indexed_time_norm = np.array([float(x) for x in re.findall(r"\[([^\]]+)\]", q)])
+                if len(indexed_time_norm) == 0:
+                    indexed_time_norm = np.array(lines[2].split(": ")[1].split(), dtype=float)
             print(f"Saved {segment_type} Indices:", sbmvmt_indices)
             print(f"Saved {segment_type} Times:", indexed_time)
-            
+            if segment_type == "Submovements":
+                print(f"Saved {segment_type} Norm Times:", indexed_time_norm)
+                
             # reindex if given time array is different from previous iteration time array
             new_sbmvmt_indices = closest_indices(time_array, indexed_time)
-            # reuse the first and last one
+            # reuse the first one
             new_sbmvmt_indices[0] = sbmvmt_indices[0]
-            new_sbmvmt_indices[-1] = sbmvmt_indices[-1]
+            # new_sbmvmt_indices[-1] = sbmvmt_indices[-1]
             print(f"New {segment_type} Indices:", new_sbmvmt_indices)
             print(f"New {segment_type} Times:", np.array(time_array[new_sbmvmt_indices],dtype=float))
             

data_visual/plot_pkg.py
@@ -171,7 +171,7 @@ def plot_multi_dim(x_list:list,data_list:list,
     for j,ax in enumerate(axs):
         # sanity check for zero crossings cuz why not
         if show_zero_cross:
-            plot_velocity_zero_crossings(x_array_i[:, j], data_list[0][:, j], ax)
+            plot_velocity_zero_crossings(x_list[0], data_list[0][:, j], ax)
     
     if legend:
         handles, labels = axs[0].get_legend_handles_labels()
@@ -297,27 +297,36 @@ def interactive_plot(fig, axs):
     fig.canvas.mpl_connect('resize_event', on_resize)
     return leg
 """ plot confidence interval with mean and std for n-lists list (each element in the list is a list of  2d arrays)"""
-def plot_stats(x:np.ndarray, data_list:list, fig=None,axs=None,labels:list=[],legend=True,relimit=False,dist="gaussian",split=4,datatype="q"):
+def plot_stats(time_list:list, data_list:list, fig=None,axs=None,labels:list=[],legend=True,relimit=False,dist="gaussian",split=4,datatype="q"):
     if "rad" in datatype:
         temp = [np.rad2deg(data) for data in data_list]
         data_list = temp
     if relimit:
-        temp_arr = np.array(data_list)
+        temp_arr = np.array(data_list[0])
+        limits = [np.min(temp_arr),np.max(temp_arr)]
+        for data in data_list:
+            temp_arr = np.array(data)
+            cur_min,cur_max = np.min(temp_arr),np.max(temp_arr)
+            if cur_min < limits[0]:
+                limits[0] = cur_min
+            if cur_max > limits[0]:
+                limits[1] = cur_max
         limits = [np.min(temp_arr),np.max(temp_arr)]
         for i, ax in enumerate(axs):
             axs[i].set_ylim(limits[0]-0.1*abs(limits[0]),limits[1]+0.1*abs(limits[1]))
 
     colors = get_n_colors(len(data_list),split=1)
-    for data,color,label in zip(data_list,colors,labels):
+    for x,data,color,label in zip(time_list,data_list,colors,labels):
+        time = x[0]
         if dist == "gaussian":
             
             mean,median,max,min,_,_ = compute_central_tendency(data)
             
             for i, ax in enumerate(axs):
                 # plot median
-                ax.plot(x, median[:, i], ls=':',color=color, label=f"{label}.median", alpha=1, linewidth=1)
+                ax.plot(time, median[:, i], ls=':',color=color, label=f"{label}.median", alpha=1, linewidth=1)
                 # Plot min max bound
-                ax.fill_between(x, min[:, i], max[:, i], 
+                ax.fill_between(time, min[:, i], max[:, i], 
                                 alpha=0.6, color=color, label=f'{label}.Bound')
         else:
             print("iqr")
@@ -326,9 +335,9 @@ def plot_stats(x:np.ndarray, data_list:list, fig=None,axs=None,labels:list=[],le
             Q3 = np.percentile(np.array(data), 75,axis=0)  # 75th percentile
 
             for i, ax in enumerate(axs):
-                ax.plot(x, Q2[:, i], ls='-',color=color, label=f"{label}.mean", alpha=0.7, linewidth=3)
+                ax.plot(time, Q2[:, i], ls='-',color=color, label=f"{label}.mean", alpha=0.7, linewidth=3)
                 # Plot 95% CI band
-                ax.fill_between(x, Q1[:, i], Q3[:, i], 
+                ax.fill_between(time, Q1[:, i], Q3[:, i], 
                                 alpha=0.3, color=color, label=f'{label}.CI95')
     if legend:
         handles, labels = axs[0].get_legend_handles_labels()
@@ -343,6 +352,7 @@ def split_plot_all(var_id_list,time_list,data_list,label_list,rep_split=4,data_t
             unique_var_id.append(f"{x}")
             
     data_list_per_var = split_reps(data_list,rep_split)
+    time_list_per_var = split_reps(time_list,rep_split)
     
     # plot gt's mean and ci for each var
     if plot:
@@ -360,7 +370,7 @@ def split_plot_all(var_id_list,time_list,data_list,label_list,rep_split=4,data_t
         )
         if stats:
             plot_stats(
-                time_list[0],
+                time_list_per_var,
                 data_list_per_var,
                 fig=fig,
                 axs=ax,
@@ -370,7 +380,7 @@ def split_plot_all(var_id_list,time_list,data_list,label_list,rep_split=4,data_t
                 datatype=data_type
             )
         interactive_plot(fig,ax)
-    return data_list_per_var,unique_var_id
+    return time_list_per_var,data_list_per_var,unique_var_id
 """ plot each axis as 1 source for spread"""
 def plot_multi_source_spread(x_list:list,data_list:list,
                            dim:int,labels:list,xtype:str,datatype:str,

lfd/tpgmm/main.py
@@ -17,23 +17,22 @@ from data_analyse.stats_pkg import compute_central_tendency
 import matplotlib.pyplot as plt
 
 plt_results = True
-retrain = False
+retrain = True
 deploy = True
-
 
 GREEN = '\033[92m'
 RED = '\033[91m'
 RESET = '\033[0m'
 
-for p in range(3,4):
+for p in range(1,2):
     if p == 1:
         sm_num = 2
     else:
         sm_num=4
         
-    for sub in range(23,25):
+    for sub in range(11,12):
         session_data = {
-            "exp_id":"exp1_trained",
+            "exp_id":"exp1",
             "patient_id":f"p{p}",
             "subject_id":f"sub{sub}",
             "sbmvmt_num":sm_num,
@@ -50,6 +49,7 @@ for p in range(3,4):
                 for train in train_list:
                     all_data.append(train["data"])
                 all_data = np.vstack(all_data)
+                print(f"Number of samples:{all_data.shape[0]}")
         
                 """
                 Transform training and test data
@@ -141,7 +141,7 @@ for p in range(3,4):
                         time_list.insert((i+1)*4-1,val_dict["time"])
                         gt_list.insert((i+1)*4-1,DataOut)
                         id_list.insert((i+1)*4-1,val_dict["id"])
-                    gt_list_per_var,unique_var_id_list = split_plot_all(var_id_list,time_list,gt_list,id_list,rep_split=4,fig_label=f"Train GT {combi_num}-{sample_num}")
+                    time_list_per_var,gt_list_per_var,unique_var_id_list = split_plot_all(var_id_list,time_list,gt_list,id_list,rep_split=4,fig_label=f"Train GT {combi_num}-{sample_num}")
                     
                     # get the mean of training samples for each variation, repeat once for the comparator as there is only 1 validation sample
                     train_comparators_per_var = []
@@ -192,7 +192,7 @@ for p in range(3,4):
                             labels=[f"{i}.GT" for i in id_list],
                             xtype="time",
                             datatype="tau",
-                            fig_label=f"Val Compare {combi_num}-{sample_num}",
+                            fig_label=f"{session_data['exp_id']} Val Compare {combi_num}-{sample_num}",
                             split=1,
                             legend=False,
                         )
@@ -203,14 +203,14 @@ for p in range(3,4):
                             labels=[f"{i}.Recon" for i in id_list],
                             xtype="time",
                             datatype="tau",
-                            fig_label=f"Val Compare {combi_num}-{sample_num}",
+                            fig_label=f"{session_data['exp_id']} Val Compare {combi_num}-{sample_num}",
                             split=1,
                             legend=False,
                             prev_fig=stats_fig,prev_ax=stats_ax,
                             shuffle=True
                         )
                         plot_stats(
-                            time_list[0],
+                            time_list_per_var,
                             gt_list_per_var,
                             fig=stats_fig,
                             axs=stats_ax,
@@ -240,8 +240,8 @@ for p in range(3,4):
                         recon_list.append(expected_data)
                         gt_list.append(DataOut)
                         id_list.append(test_dict["id"])
-                    gt_list_per_var,unique_var_id_list = split_plot_all(var_id_list,time_list,gt_list,id_list,rep_split=4,fig_label=f"Test GT {combi_num}-{sample_num}")
-                    recon_list_per_var,_ = split_plot_all(var_id_list,time_list,recon_list,id_list,rep_split=4,fig_label="Test Recon {combi_num}-{sample_num}")
+                    time_list_per_var,gt_list_per_var,unique_var_id_list = split_plot_all(var_id_list,time_list,gt_list,id_list,rep_split=4,fig_label=f"Test GT {combi_num}-{sample_num}")
+                    _,recon_list_per_var,_ = split_plot_all(var_id_list,time_list,recon_list,id_list,rep_split=4,fig_label="Test Recon {combi_num}-{sample_num}")
                     
                     # get the mean of each test validation, and repeat for 4 repetitions
                     test_comparators_per_var = []
@@ -273,7 +273,7 @@ for p in range(3,4):
                             labels=[f"{x}.Test GT" for x in id_list],
                             xtype="time (s)",
                             datatype="tau",
-                            fig_label=f"Test Compare {combi_num}-{sample_num}",
+                            fig_label=f"{session_data['exp_id']} Test Compare {combi_num}-{sample_num}",
                             split=4,
                             legend=False
                         )
@@ -285,7 +285,7 @@ for p in range(3,4):
                 
                             xtype="time",
                             datatype="tau",
-                            fig_label=f"Test Compare {combi_num}-{sample_num}",
+                            fig_label=f"{session_data['exp_id']} Test Compare {combi_num}-{sample_num}",
                             split=4,
                             legend=False,
                             prev_fig=stats_fig,
@@ -293,7 +293,7 @@ for p in range(3,4):
                             shuffle=True
                         )
                         plot_stats(
-                            time_list[0],
+                            time_list_per_var,
                             gt_list_per_var,
                             fig=stats_fig,
                             axs=stats_ax,