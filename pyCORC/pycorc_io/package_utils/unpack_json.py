a40743875f6f889878ab75e29d8da24149a0ec61
jqloh <jqloh@student.unimelb.edu.au>
2026-07-17 10:31:03 +1000

    split subject file and subject params code

--- pyCORC/pycorc_io/package_utils/unpack_json.py
+++ pyCORC/pycorc_io/package_utils/unpack_json.py
@@ -1,24 +1,28 @@
 import json
 import numpy as np
 
-def get_subject_params(subject_path):
+def get_subject_file(subject_path):
     json_path = f"{subject_path}/body_meas.json"
     with open(json_path, 'r') as file:
-        body_params = json.load(file)
-        body_params_rbt = {'torso': body_params["torso"]/1000,
-                        'clav': body_params["clav"]/1000,
-                        'ua_l': body_params["ua_l"]/1000,
-                        'fa_l': body_params["fa_l"]/1000,
-                        'ha_l': body_params["ha_l"]/1000,
-                        'm_ua': 2.0,
-                        'm_fa': 1.1+0.23+0.6,
-                        "shoulder_aa_offset": np.array(body_params["shoulder_aa_offset"]),
-                        "ft_offsets": body_params["ft_offsets"]}
+        body_file = json.load(file)
+    return body_file
         
-        gravity ={
-          "clav": body_params["cuff_grav"]["clav"]+body_params["sensor_grav"],
-          "ua":   body_params["cuff_grav"]["ua"]+body_params["sensor_grav"],
-          "fa":   body_params["cuff_grav"]["fa"]+body_params["sensor_grav"]
-        }
+def get_subject_params(subject_path):
+    body_params = get_subject_file(subject_path)
+    body_params_rbt = {'torso': body_params["torso"]/1000,
+                    'clav': body_params["clav"]/1000,
+                    'ua_l': body_params["ua_l"]/1000,
+                    'fa_l': body_params["fa_l"]/1000,
+                    'ha_l': body_params["ha_l"]/1000,
+                    'm_ua': 2.0,
+                    'm_fa': 1.1+0.23+0.6,
+                    "shoulder_aa_offset": np.array(body_params["shoulder_aa_offset"]),
+                    "ft_offsets": body_params["ft_offsets"]}
+    
+    gravity ={
+      "clav": body_params["cuff_grav"]["clav"]+body_params["sensor_grav"],
+      "ua":   body_params["cuff_grav"]["ua"]+body_params["sensor_grav"],
+      "fa":   body_params["cuff_grav"]["fa"]+body_params["sensor_grav"]
+    }
         
     return body_params_rbt,gravity,body_params["removed_bias"]
